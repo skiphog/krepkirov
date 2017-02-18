c911164d38ab4cc3901fda96ac6b5fd1627dbcf3
Skip <skiphog@mail.ru>
2017-02-18 19:15:47 +0300

    Add page contacts and search

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,15 @@ Route::get('/', function () {
     return view('pages.index');
 });
 
-Route::get('/certificates',function (){
+Route::get('/certificates', function () {
     return view('pages.certificates');
 });
 
-Route::get('/contacts',function (){
+Route::get('/contacts', function () {
     return view('pages.contacts');
 });
 
-Route::get('/prices','PriceController@index');
+Route::get('/prices', 'PriceController@index');
 
 Route::group(['prefix' => 'admin'], function () {
 
@@ -29,39 +29,39 @@ Route::group(['prefix' => 'admin'], function () {
 
     Route::resource('/categories', 'Admin\CategoryController');
 
-    Route::get('/products','Admin\ProductController@index');
+    Route::get('/products', 'Admin\ProductController@index');
 
-    Route::get('/products/search','Admin\ProductController@search');
+    Route::get('/products/search', 'Admin\ProductController@search');
 
-    Route::get('/products/get','Admin\ProductController@getProductOnCategory');
+    Route::get('/products/get', 'Admin\ProductController@getProductOnCategory');
 
-    Route::post('/products/sort','Admin\ProductController@saveSortProduct');
+    Route::post('/products/sort', 'Admin\ProductController@saveSortProduct');
 
-    Route::post('/products/destroy','Admin\ProductController@destroy');
+    Route::post('/products/destroy', 'Admin\ProductController@destroy');
 
-    Route::get('/products/{product}/edit','Admin\ProductController@edit');
+    Route::get('/products/{product}/edit', 'Admin\ProductController@edit');
 
-    Route::put('/products/{product}','Admin\ProductController@update');
+    Route::put('/products/{product}', 'Admin\ProductController@update');
 
-    Route::get('/price','Admin\PriceController@index');
+    Route::get('/price', 'Admin\PriceController@index');
 
-    Route::post('/price/upload','Admin\PriceController@upload');
+    Route::post('/price/upload', 'Admin\PriceController@upload');
 
 });
 
 Route::group(['prefix' => 'cart'], function () {
 
-    Route::post('/change','CartController@change');
+    Route::post('/change', 'CartController@change');
 
-    Route::post('/clear','CartController@clear');
+    Route::post('/clear', 'CartController@clear');
 
-    Route::get('/show','CartController@show');
+    Route::get('/show', 'CartController@show');
 
 });
 
-Route::group(['prefix' => 'account'], function (){
+Route::group(['prefix' => 'account'], function () {
 
-    Route::post('/order','OrderController@store')->middleware('cart');
+    Route::post('/order', 'OrderController@store')->middleware('cart');
 
 });
 
